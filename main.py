7e5787ceb9ffdce3c77117d65b8d795d65900967
akshajraju0505 <97037481+akshajraju0505@users.noreply.github.com>
2025-07-27 02:53:08 +0530

    adding the new frontend url to backend

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ logger = logging.getLogger(__name__)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
